964a0a6ba2a803c4b9691282f438d9c85180e75a
Sathancabrol <124669348+Sathancabrol@users.noreply.github.com>
2026-09-07 05:10:35 +0200

    feat(watchtower): expose OSINT module entrypoint

---
+++ watchtower-mods/src/osint/index.js
@@ -0,0 +1,4 @@
+export { initOsintWorkbench } from './osintWorkbench.js';
+export { OSINT_SOURCES, getOsintSource, listOsintSources } from './osintRegistry.js';
+export { EVIDENCE_TYPES, createObservation, canPromoteToFact } from './osintEvidence.js';
+export { createInvestigation, addEntity, addRelation } from './osintCase.js';
